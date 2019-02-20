c5fe60cb2e28752281ce44776119b97b163c418c
Manny <coachmannytw@gmail.com>
2019-02-19 22:53:32 -0700

    multiple changes

---
+++ src/templates/pageTemplate.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+export default ({pageContext}) => (
+    <div>
+        <h1>
+            {pageContext.title}
+        </h1>
+    </div>
+
+)
---
+++ src/templates/postTemplate.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+export default ({pageContext}) => (
+    <div>
+        <h1>
+            {pageContext.title}
+        </h1>
+    </div>
+
+)
+
