a6d31b399e86e6cd0b1ccc3e681406fca2f5ac75
Manny <coachmannytw@gmail.com>
2019-03-09 07:54:34 -0700

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
           "**/users",
         ],
         // Blacklisted routes using glob patterns
-        excludedRoutes: ["**/posts/1456"],
+        excludedRoutes: [],
         // use a custom normalizer which is applied after the built-in ones.
         normalizer: function({ entities }) {
           return entities
